f5a3920abd90562de7d5ba8f1d87b613e68b939f
Ayushi Chitaliya <ayushichitaliya30gmail.com>
2024-09-21 10:43:17 +0530

    Done1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import Mainbody from './Componets/Mainbody'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
