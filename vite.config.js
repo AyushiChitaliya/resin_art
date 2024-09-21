9800aaa21229c79da994985fe55391ecb62f108c
Ayushi Chitaliya <ayushichitaliya30gmail.com>
2024-09-21 10:49:00 +0530

    Done1

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      'slick-carousel/slick/slick.css',
+      'slick-carousel/slick/slick-theme.css'
+    ],
+  },
   build: {
     rollupOptions: {
-      external: ['slick-carousel'],
+      external: ['slick-carousel/slick/slick.css', 'slick-carousel/slick/slick-theme.css'],
     },
   },
-})
+});
